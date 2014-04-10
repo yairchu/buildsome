7d2d8b6be3d8259b1ce7818b5bbee67b7aa2ed02
Eyal Lotem <eyal.lotem@gmail.com>
2014-04-11 00:31:01 +0300

    Parallelism: Track threads that allocate parallelism

--- src/Lib/Parallelism.hs
+++ src/Lib/Parallelism.hs
@@ -6,41 +6,56 @@ module Lib.Parallelism
   , withReleased
   ) where
 
-import Control.Applicative ((<$>))
+import Control.Applicative (Applicative(..), (<$>))
+import Control.Concurrent (ThreadId, myThreadId)
 import Data.IORef
+import Data.Map (Map)
+import Lib.IORef (atomicModifyIORef'_)
 import Lib.PoolAlloc (PoolAlloc)
 import qualified Control.Exception as E
+import qualified Data.Map as M
 import qualified Lib.PoolAlloc as PoolAlloc
 
 type ParId = Int
 type Cell = IORef ParId
 
-newtype Parallelism = Parallelism
-  { parPool :: PoolAlloc ParId
+data Parallelism = Parallelism
+  { _parPool :: PoolAlloc ParId
+  , _parThreads :: IORef (Map ParId ThreadId)
   }
 
 new :: ParId -> IO Parallelism
-new n = Parallelism <$> PoolAlloc.new [1..n]
+new n = Parallelism <$> PoolAlloc.new [1..n] <*> newIORef M.empty
+
+addMyThreadId :: IORef (Map ParId ThreadId) -> ParId -> IO ()
+addMyThreadId threads parId = do
+  threadId <- myThreadId
+  atomicModifyIORef'_ threads $ M.insert parId threadId
 
 startAlloc :: Parallelism -> IO ((Cell -> IO r) -> IO r)
-startAlloc parallelism@(Parallelism pool) = do
+startAlloc parallelism@(Parallelism pool threads) = do
   alloc <- PoolAlloc.startAlloc pool
-  return $ E.bracket (newIORef =<< alloc) (release parallelism)
+  return $ E.bracket (newCell =<< alloc) (release parallelism)
+  where
+    newCell parId = do
+      addMyThreadId threads parId
+      newIORef parId
 
 release :: Parallelism -> Cell -> IO ()
-release (Parallelism pool) cell = do
+release (Parallelism pool threads) cell = do
   parId <- readIORef cell
   E.mask_ $ do
     PoolAlloc.release pool parId
+    atomicModifyIORef'_ threads $ M.delete parId
     writeIORef cell $ error "Attempt to read released resource"
 
 -- | Release the currently held item, run given action, then regain
 -- new item instead
-localReleasePool :: Parallelism -> Cell -> IO b -> IO b
-localReleasePool parallelism cell =
-  E.bracket_
-  (release parallelism cell)
-  (writeIORef cell =<< PoolAlloc.alloc (parPool parallelism))
-
-withReleased :: Cell -> Parallelism -> IO a -> IO a
-withReleased cell parallelism = localReleasePool parallelism cell
+withReleased :: Cell -> Parallelism -> IO b -> IO b
+withReleased cell parallelism@(Parallelism pool threads) =
+  E.bracket_ (release parallelism cell) alloc
+  where
+    alloc = E.mask_ $ do
+      parId <- PoolAlloc.alloc pool
+      addMyThreadId threads parId
+      writeIORef cell parId
