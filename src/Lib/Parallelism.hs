f5620258d30a16d2dc6639f7c9ca7cb4a8d69e21
Eyal Lotem <eyal.lotem@gmail.com>
2014-04-11 01:01:07 +0300

    Add Parallelism.kill

--- src/Lib/Parallelism.hs
+++ src/Lib/Parallelism.hs
@@ -4,10 +4,12 @@ module Lib.Parallelism
   , Cell
   , startAlloc
   , withReleased
+  , kill
   ) where
 
 import Control.Applicative (Applicative(..), (<$>))
-import Control.Concurrent (ThreadId, myThreadId)
+import Control.Concurrent (ThreadId, myThreadId, killThread)
+import Control.Monad (join)
 import Data.IORef
 import Data.Map (Map)
 import Lib.IORef (atomicModifyIORef'_)
@@ -19,43 +21,55 @@ import qualified Lib.PoolAlloc as PoolAlloc
 type ParId = Int
 type Cell = IORef ParId
 
+data ParallelismState = Live (Map ParId ThreadId) | Killed
+
 data Parallelism = Parallelism
-  { _parPool :: PoolAlloc ParId
-  , _parThreads :: IORef (Map ParId ThreadId)
+  { parPool :: PoolAlloc ParId
+  , _parState :: IORef ParallelismState
   }
 
 new :: ParId -> IO Parallelism
-new n = Parallelism <$> PoolAlloc.new [1..n] <*> newIORef M.empty
+new n = Parallelism <$> PoolAlloc.new [1..n] <*> newIORef (Live M.empty)
 
-addMyThreadId :: IORef (Map ParId ThreadId) -> ParId -> IO ()
-addMyThreadId threads parId = do
+finalizeAlloc :: Parallelism -> ParId -> IO ()
+finalizeAlloc (Parallelism pool parState) parId = do
   threadId <- myThreadId
-  atomicModifyIORef'_ threads $ M.insert parId threadId
+  join $ atomicModifyIORef' parState $ \old -> case old of
+    Live m -> (Live (M.insert parId threadId m), return ())
+    Killed -> (Killed, PoolAlloc.release pool parId >> E.throwIO E.ThreadKilled)
 
 startAlloc :: Parallelism -> IO ((Cell -> IO r) -> IO r)
-startAlloc parallelism@(Parallelism pool threads) = do
-  alloc <- PoolAlloc.startAlloc pool
+startAlloc parallelism = do
+  alloc <- PoolAlloc.startAlloc $ parPool parallelism
   return $ E.bracket (newCell =<< alloc) (release parallelism)
   where
     newCell parId = do
-      addMyThreadId threads parId
+      finalizeAlloc parallelism parId
       newIORef parId
 
 release :: Parallelism -> Cell -> IO ()
-release (Parallelism pool threads) cell = do
+release (Parallelism pool parState) cell = do
   parId <- readIORef cell
   E.mask_ $ do
     PoolAlloc.release pool parId
-    atomicModifyIORef'_ threads $ M.delete parId
+    atomicModifyIORef'_ parState $ \old -> case old of
+      Live m -> Live (M.delete parId m)
+      Killed -> Killed
     writeIORef cell $ error "Attempt to read released resource"
 
 -- | Release the currently held item, run given action, then regain
 -- new item instead
 withReleased :: Cell -> Parallelism -> IO b -> IO b
-withReleased cell parallelism@(Parallelism pool threads) =
+withReleased cell parallelism =
   E.bracket_ (release parallelism cell) alloc
   where
     alloc = E.mask_ $ do
-      parId <- PoolAlloc.alloc pool
-      addMyThreadId threads parId
+      parId <- PoolAlloc.alloc $ parPool parallelism
+      finalizeAlloc parallelism parId
       writeIORef cell parId
+
+kill :: Parallelism -> IO ()
+kill (Parallelism _ parState) =
+  join $ atomicModifyIORef' parState $ \old -> case old of
+  Live m -> (Killed, mapM_ killThread $ M.elems m)
+  Killed -> (Killed, return ())
