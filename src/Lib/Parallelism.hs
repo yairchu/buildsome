fa3581d97d61c0e89ea8f6551fd9770d0a3e0251
Eyal Lotem <eyal.lotem@gmail.com>
2014-04-10 20:44:22 +0300

    Parallelism newtype

--- src/Lib/Parallelism.hs
+++ src/Lib/Parallelism.hs
@@ -6,6 +6,7 @@ module Lib.Parallelism
   , withReleased
   ) where
 
+import Control.Applicative ((<$>))
 import Data.IORef
 import Lib.PoolAlloc (PoolAlloc)
 import qualified Control.Exception as E
@@ -13,21 +14,24 @@ import qualified Lib.PoolAlloc as PoolAlloc
 
 type ParId = Int
 type Cell = IORef ParId
-type Parallelism = PoolAlloc ParId
+
+newtype Parallelism = Parallelism
+  { parPool :: PoolAlloc ParId
+  }
 
 new :: ParId -> IO Parallelism
-new n = PoolAlloc.new [1..n]
+new n = Parallelism <$> PoolAlloc.new [1..n]
 
 startAlloc :: Parallelism -> IO ((Cell -> IO r) -> IO r)
-startAlloc parallelism = do
-  alloc <- PoolAlloc.startAlloc parallelism
+startAlloc parallelism@(Parallelism pool) = do
+  alloc <- PoolAlloc.startAlloc pool
   return $ E.bracket (newIORef =<< alloc) (release parallelism)
 
 release :: Parallelism -> Cell -> IO ()
-release parallelism cell = do
+release (Parallelism pool) cell = do
   parId <- readIORef cell
   E.mask_ $ do
-    PoolAlloc.release parallelism parId
+    PoolAlloc.release pool parId
     writeIORef cell $ error "Attempt to read released resource"
 
 -- | Release the currently held item, run given action, then regain
@@ -36,7 +40,7 @@ localReleasePool :: Parallelism -> Cell -> IO b -> IO b
 localReleasePool parallelism cell =
   E.bracket_
   (release parallelism cell)
-  (writeIORef cell =<< PoolAlloc.alloc parallelism)
+  (writeIORef cell =<< PoolAlloc.alloc (parPool parallelism))
 
 withReleased :: Cell -> Parallelism -> IO a -> IO a
 withReleased cell parallelism = localReleasePool parallelism cell
